edc4bc730325d29c6edb5f873025004d42943cee
priyanshuraj09 <priyanshuverma6891@gmail.com>
2025-05-16 00:45:12 +0530

    cnanges in Hera jsx

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -39,7 +39,7 @@ const Hero = () => {
           </div>
           <div data-aos="fade-up" className="buttons flex gap-5">
             <a
-              href=""
+              href="#contact"
               className=" bg-black text-[1rem] text-white px-10 py-2 sm:px-8 rounded-lg font-bold  hover:text-yellow-500"
             >
               <span> Hire Me</span>
