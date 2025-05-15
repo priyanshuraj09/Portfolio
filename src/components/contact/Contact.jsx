131d5e2fa9d18f4e4cc232b6bee0025420ad0a45
priyanshuraj09 <priyanshuverma6891@gmail.com>
2025-05-16 00:19:22 +0530

    first commit

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,116 @@
+import React from "react";
+import { BsArrowRight } from "react-icons/bs";
+import { RiSendPlaneFill } from "react-icons/ri";
+
+const Contact = () => {
+
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "6ba92873-1ab3-4865-8b65-f908a88994ed");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
+
+
+
+
+  
+  return (
+    <div id="contact" className="container m-auto mt-16">
+      {/* heading */}
+      <div 
+      // data-aos="fade-up"
+       className="relative mb-5">
+        <h3 className=" text-3xl font-black text-gray-400 sm:text-2xl">
+          Contact
+        </h3>
+        <span className="h-[1.1px] right-0 absolute w-[92%] bg-gray-300 block"></span>
+      </div>
+
+      {/* card*/}
+      <div className="card-wrapper w-[90%] sm:w-[100%] mx-auto mt-5 flex items-center justify-center sm:flex-col">
+        <div className="left w-[70%] flex-1 flex items-center justify-center sm:flex-col sm:w-full">
+          <div className="flex-3 w-1/2 gap-3 flex items-end justify-end  flex-col sm:w-3/4">
+            <div 
+            data-aos="zoom-in"
+            >
+              <h1 className="text-5xl font-bold sm:text-3xl">For Hiring</h1>
+              <h1 className="text-3xl font-bold sm:text-3xl">And Contact</h1>
+              <h3 className="text-xl sm:text-lg">
+                Kindly Mail here
+              </h3>
+            </div>
+          </div>
+          <div className=" flex p-5 items-center justify-center ">
+            <button
+              
+              data-aos="zoom-in"
+              
+              className= " text-yellow-500 font-extrabold text-3xl p-2 rounded-lg shadow-[0_0_10px_1px_rgba(0,0,0,0.1)] "
+            >
+              <BsArrowRight className=" md:rotate-90" />
+            </button>
+          </div>
+        </div>
+        <div className="right flex-1">
+          <form
+          onSubmit={onSubmit}
+            
+            data-aos="zoom-in"
+            
+            className="flex justify-center items-center flex-col gap-5 w-[70%] md:w-[100%] sm:w-[95%] mx-auto"
+            action="mailto:antitgolu123@gmail.com"
+          >
+            <input
+              className="px-3 shadow-[0_0_16px_0px_rgba(0,0,0,0.1)] p-2 rounded-lg w-full"
+              type="email"
+              placeholder="e.g. example@email.com"
+              name=""
+            />
+            <input
+              className="px-3 shadow-[0_0_16px_0px_rgba(0,0,0,0.1)] p-2 rounded-lg w-full"
+              type="text"
+              placeholder="e.g. Rohit Wagh"
+              name=""
+            />
+            <textarea
+              className="px-3 shadow-[0_0_16px_0px_rgba(0,0,0,0.1)] p-2 rounded-lg w-full"
+              rows="4"
+              cols="50"
+              placeholder="Write your message"
+              name=""
+              id=""
+            />
+            <button
+              className="bg-yellow-500 w-full text-white font-semibold  p-2 rounded-lg flex items-center justify-center space-x-1"
+              type="submit"
+            >
+             <button type="submit">Submit now</button>
+              <RiSendPlaneFill/>
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
---
+++ src/data/data.js
@@ -0,0 +1,25 @@
+export const data = [
+  {
+    id: 1,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRMH9-0hxzhTvWtbMcgqGo8_rkpOurt_BTdg&s",
+    title: "Weather Forecasting Api",
+    link: "",
+    git: "",
+  },
+  {
+    id: 2,
+    img: "https://imgs.search.brave.com/m_uDQjui3XrwU9JsIJSFX6eS2pxKePmwI8aSyRhCFiU/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/cHJlbWl1bS1waG90/by95b3VuZy1zdHVk/ZW50LXVzaW5nLWNv/bXB1dGVyLXN0dWR5/aW5nLW9ubGluZS1l/ZHVjYXRpb24tb25s/aW5lLWxlYXJuaW5n/XzE0MjEtNDU2My5q/cGc_c2VtdD1haXNf/aHlicmlkJnc9NzQw",
+    title: "Educational Website",
+    desc: "",
+    link: "https://edusity-project-seven.vercel.app/",
+    git: "https://github.com/Ankitkr-001/edusite_reactProject1",
+  },
+  {
+    id: 3,
+    img: "https://imgs.search.brave.com/F-4B99KCeR2FKKrFVnSHWYAkRVboXvJuixpIRZNS_O4/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/aG9zdGluZ2VyLmNv/bS90dXRvcmlhbHMv/d3AtY29udGVudC91/cGxvYWRzL3NpdGVz/LzIvMjAyMi8wNC93/ZWItZGV2ZWxvcGVy/LXBvcnRmb2xpby53/ZWJw",
+    title: "Portfolio",
+    desc: "",
+    link: "",
+    git: "",
+  },
+];
